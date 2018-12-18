da300016547f0c56efb2c281b6f2173e80f76300
Keven van Zuijlen <kevenvz1@gmail.com>
2018-12-18 22:15:05 +0100

    Disabled SSL as this will break HMR

--- AspNetCoreVueBoilerplate/Startup.cs
+++ AspNetCoreVueBoilerplate/Startup.cs
@@ -46,9 +46,9 @@ namespace AspNetCoreVueBoilerplate
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
+            }
 
             app.UseSpaStaticFiles();
 
