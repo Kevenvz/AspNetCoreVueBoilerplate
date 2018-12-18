7518b853a3bfd5218c9fd89f0f7df41308ab9d10
Keven van Zuijlen <kevenvz1@gmail.com>
2018-12-18 20:34:15 +0100

    Added serilog for logging

--- AspNetCoreVueBoilerplate/Program.cs
+++ AspNetCoreVueBoilerplate/Program.cs
@@ -7,18 +7,47 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace AspNetCoreVueBoilerplate
 {
     public static class Program
     {
+        public static readonly IConfiguration Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static Task Main(string[] args)
         {
-            return CreateWebHostBuilder(args).Build().RunAsync();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            try
+            {
+                Log.Information("Starting the webhost...");
+                return CreateWebHostBuilder(args).Build().RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseConfiguration(Configuration)
+                .UseSerilog();
     }
 }
